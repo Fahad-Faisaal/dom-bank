5b94c31710e95f4ecdb7786917ae759b2a087707
Fahad-Faisaal <fahads1ab@gmail.com>
2022-02-10 23:46:45 +0600

    helping function for getting inputs added

--- scripts/bank.js
+++ scripts/bank.js
@@ -1,46 +1,49 @@
+// Helping Functions
+function getInputValue(id) {
+  const input = document.getElementById(id);
+  const amount = parseFloat(input.value);
+  input.value = '';
+  return amount;
+} 
+
 // Handler for deposits
 document.getElementById('btn-deposit').addEventListener('click', function() {
-  // -----------------Getting deposit amount---------------------
-  const depositInput = document.getElementById('deposit-input');
-  const depositAmount = parseFloat(depositInput.value);
+  // Getting deposit amount
+  const depositAmount = getInputValue('deposit-input');
   
-  // ----------------Get current deposit----------------------
+  // Get current deposit
   const currDeposit = document.getElementById('deposit-total');
   const currDepositAmount = parseFloat(currDeposit.innerText);
   // Adding deposits
   const totalDeposit = currDepositAmount + depositAmount;
   currDeposit.innerText = totalDeposit;
 
-  // --------------------updating balance--------------------
+  // updating balance
   const currBalance = document.getElementById('balance-total');
   const currBalanceAmount = parseFloat(currBalance.innerText);
   const totalBalance = currBalanceAmount + depositAmount;
   currBalance.innerText = totalBalance;
-
-  // ---------------Clearing deposit input field---------------
-  depositInput.value = '';
 });
 
 
 // handler for withdraw
 document.getElementById('btn-withdraw').addEventListener('click', function() {
-  // -----------------Getting withdraw amount--------------------
-  const withdrawInput = document.getElementById('withdraw-input');
-  const withdrawAmount = parseFloat(withdrawInput.value);
+  // Getting withdraw amount
+  const withdrawAmount = getInputValue('withdraw-input');
   
-  // --------------Getting current withdraws---------------
+  // Getting current withdraws
   const currWithdraw = document.getElementById('withdraw-total');
   const currWithdrawAmount = parseFloat(currWithdraw.innerText);
   // Adding withdraw
   const totalWithdraw = currWithdrawAmount + withdrawAmount;
   currWithdraw.innerText = totalWithdraw;
 
-  // ---------------updating balance-----------------
+  // updating balance
   const currBalance = document.getElementById('balance-total');
   const currBalanceAmount = parseFloat(currBalance.innerText);
   const totalBalance = currBalanceAmount - withdrawAmount;
   currBalance.innerText = totalBalance;
 
-  // ----------------clearing fields----------------
+  // clearing fields
   withdrawInput.value = '';
 });
