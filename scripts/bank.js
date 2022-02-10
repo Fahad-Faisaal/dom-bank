a57ce17ed100f588a7ff901e7f764b26ddefda75
Fahad-Faisaal <fahads1ab@gmail.com>
2022-02-11 00:13:10 +0600

    helping function for total deposit and withdraw added

--- scripts/bank.js
+++ scripts/bank.js
@@ -6,17 +6,20 @@ function getInputValue(id) {
   return amount;
 } 
 
+function addTransactions (id, amount) {
+  const currTransaction = document.getElementById(id);
+  const currTransactionAmount = parseFloat(currTransaction.innerText);
+  const totalTransaction = currTransactionAmount + amount;
+  currTransaction.innerText = totalTransaction;
+}
+
 // Handler for deposits
 document.getElementById('btn-deposit').addEventListener('click', function() {
   // Getting deposit amount
   const depositAmount = getInputValue('deposit-input');
   
-  // Get current deposit
-  const currDeposit = document.getElementById('deposit-total');
-  const currDepositAmount = parseFloat(currDeposit.innerText);
-  // Adding deposits
-  const totalDeposit = currDepositAmount + depositAmount;
-  currDeposit.innerText = totalDeposit;
+  // Updating total deposit
+  addTransactions('deposit-total', depositAmount);
 
   // updating balance
   const currBalance = document.getElementById('balance-total');
@@ -25,25 +28,17 @@ document.getElementById('btn-deposit').addEventListener('click', function() {
   currBalance.innerText = totalBalance;
 });
 
-
 // handler for withdraw
 document.getElementById('btn-withdraw').addEventListener('click', function() {
   // Getting withdraw amount
   const withdrawAmount = getInputValue('withdraw-input');
   
-  // Getting current withdraws
-  const currWithdraw = document.getElementById('withdraw-total');
-  const currWithdrawAmount = parseFloat(currWithdraw.innerText);
-  // Adding withdraw
-  const totalWithdraw = currWithdrawAmount + withdrawAmount;
-  currWithdraw.innerText = totalWithdraw;
+  // Updating total withdraw
+  addTransactions('withdraw-total', withdrawAmount);
 
   // updating balance
   const currBalance = document.getElementById('balance-total');
   const currBalanceAmount = parseFloat(currBalance.innerText);
   const totalBalance = currBalanceAmount - withdrawAmount;
   currBalance.innerText = totalBalance;
-
-  // clearing fields
-  withdrawInput.value = '';
 });
